ceaa702925b595791bd301da1f47f8d478122305
philmo <phil_27mo@hotmail.com>
2019-02-01 15:44:04 -0500

    google api plug in

--- app/src/main/java/com/example/darsihmadapp/MainActivity.java
+++ app/src/main/java/com/example/darsihmadapp/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.darsihmadapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
