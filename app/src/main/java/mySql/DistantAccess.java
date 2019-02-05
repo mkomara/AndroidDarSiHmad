552a206ddb716d9a2aa49fcd42122404133f743f
philippe <phil_27mo@hotmail.com>
2019-02-05 15:26:01 -0500

    php plus processfinish

--- app/src/main/java/mySql/DistantAccess.java
+++ app/src/main/java/mySql/DistantAccess.java
@@ -1,12 +1,16 @@
 package mySql;
 
+import android.util.Log;
+
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class DistantAccess implements AsyncResponse {
 
     //change the string value
   private final String SERVERADDR = "http://serverIpAdress/folder/file.php";
-
+// private User user;
   public DistantAccess(){
       super();
   }
@@ -21,8 +25,14 @@ public class DistantAccess implements AsyncResponse {
     dataAccess.execute(SERVERADDR);
   }
 
-  public void requestUser(String name, String password){
+  public void requestUser(String operation, String name){
+      AccessHTTP dataAccess = new AccessHTTP();
 
+      dataAccess.setDelegate(this);
+
+      dataAccess.addParam(operation,name);
+
+      dataAccess.execute(SERVERADDR);
   }
 
 
@@ -30,6 +40,22 @@ public class DistantAccess implements AsyncResponse {
 
     @Override
     public void processFinish(String output) {
+        Log.d("response",output);
+        String[] response = output.split("%");
+        if(response != null){
+            if(response[0] == "User"){
+                try {
+                    JSONObject infos = new JSONObject(response[1]);
+                    String Name = infos.getString("name");
+                    String password = infos.getString("password");
+                    String role = infos.getString("role_id");
+                    //User user = new User(Name,password,role);
+                    //this.setUser(user);
 
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- php/function.php
+++ php/function.php
@@ -11,9 +11,8 @@ function Connexion(){
  		print("failed");
 		die('connection failed: '.$con->connect_error);
 	}
-	else{
-		print("connection réeussi!");
-	}
+	return $con;
+}
 
 
 
@@ -21,8 +20,17 @@ function InsertData(){
 
 }
 
-function RequestUser($userName){
-
+function RequestUser($userName'$connection){
+	$Query = "SELECT name,password,role_id FROM users where name='$userName'";
+	$result = $connection->query($Query);
+	if($result->num_rows > 0){
+ 		while ( $row = $result->fetch_assoc()) {
+ 			print(" User% name:".$row["name"]."password:".$row["password"]."role_id:".$row["role_id"]);
+ 		}
+ 	}
+	else{
+		print("failed to get user");
+	}
 }
 
 ?>
